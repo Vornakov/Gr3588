a3dedea3290fdbe6ba434d58e7a48e535d07a241
Алексей <vornakov39@gmail.com>
2022-12-30 19:30:16 +0300

    add solution for tasks 54,56,60,62

---
+++ Sem8Task62/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая заполнит спирально массив 4 на 4
+
+ //Заполняем массив порядковыми числами по спирали
+ void FillSpiral(int[,] matrix)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+ }
+
+ //Печать двумерного массива
+ void Print2DArray(int[,] matrix)
+ {
+     for (int i = 0; i < matrix.GetLength(0); i++)
+     {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+         {
+             Console.Write(matrix[i, j]+"     ".Substring(matrix[i, j].ToString().Length));
+         }
+         Console.WriteLine();
+     }
+     Console.WriteLine();
+ }
+
+int[,] matrix = new int[4, 4];
+FillSpiral(matrix);
+Print2DArray(matrix);
