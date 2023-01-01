b635530be9fb03b6045e71983d354b51f3eb8236
Алексей <vornakov39@gmail.com>
2023-01-01 18:11:19 +0300

    add solution 64,66,68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+ int ReadData(string line)
+ {
+     Console.Write(line);
+     int number = int.Parse(Console.ReadLine() ?? "0");
+     return number;
+ }
+
+ // Печать результата
+ void PrintResult(int prefix)
+ {
+     Console.WriteLine(prefix);
+ }
+
+ //Метод поиска суммы натуральных элементов в промежутке заданных значений с помощью рекурсии
+ int SumMNRec(int M, int N)
+ {
+    if(M >= N) return N;
+    return M + SumMNRec(M+1, N);
+ }
+
+ int number1 = ReadData("Введите число M: ");
+ int number2 = ReadData("Введите число N: ");
+ int resultLine = SumMNRec(number1, number2);
+ PrintResult(resultLine);
